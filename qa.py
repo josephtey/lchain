f8a0b7a881085983746b5ce4af6d3a8ecff9a605
Joseph Tey <jjttey@gmail.com>
2023-03-04 14:52:04 -0800

    base

--- qa.py
+++ qa.py
@@ -11,7 +11,7 @@ with open("faiss_store.pkl", "rb") as f:
 
 store.index = index
 
-chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+chain = VectorDBQAWithSourcesChain.from_chain_type(llm=OpenAI(temperature=0), vectorstore=store)
 
 while True:
   question = input("Question: ")
@@ -19,9 +19,17 @@ while True:
   print(f"Answer: {result['answer']}")
   print(f"All sources: {result['sources']}")
 
-  main_source = result['sources'].split(",")[-1]
-  main_source_file_name = main_source.split("<split>")[0]
-  main_source_index = int(main_source.split("<split>")[1])
-  corresponding_docstore = store.index_to_docstore_id[main_source_index]
+  all_sources = result['sources'].replace(";", ",").split(",")
+  all_docs = []
 
-  print(f"Source: {store.docstore.search(store.index_to_docstore_id[main_source_index]).page_content}")
+  for source in all_sources:
+    file_name = source.split("<split>")[0]
+    index = int(source.split("<split>")[1])
+    corresponding_docstore = store.index_to_docstore_id[index]
+    doc = store.docstore.search(corresponding_docstore).page_content
+
+    all_docs.append(doc)
+
+  for doc in all_docs:
+     print(doc)
+     print("\n")
